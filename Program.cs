ad6d754bc4c132a9af57d2fbc5a95e654332bb3c
Kassu <Kassu@SKOLDATORN>
2025-10-21 15:42:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+
+
+namespace ChuckNorrisAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            string apiUrl = "https://api.chucknorris.io/jokes/random";
+
+            Console.WriteLine("Fetching a joke....");
+
+            var responce = await client.GetAsync(apiUrl);
+
+            responce.EnsureSuccessStatusCode();
+
+            string json= await responce.Content.ReadAsStringAsync();
+
+            var joke = JsonSerializer.Deserialize<Joke>(json);
+
+
+
+
+            List<Joke>listOfJokes=new List<Joke>();
+            listOfJokes.Add(joke);
+        }
+    }
+}
